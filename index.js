2ebd0f48848a8c03333c3f948713da4b3ce4d7ad
attila126 <89414587+attila126@users.noreply.github.com>
2021-08-23 20:29:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const Discord = require("discord.js");
-const tokenfile = require("./tokenfile.json");
 const botconfig = require("./botconfig.json");
 const bot = new Discord.Client({disableEveryone: true});
 
@@ -24,4 +23,4 @@ bot.on("ready", async() => {
 
 
 
-bot.login(tokenfile.token);
+bot.login(process.env.BOT_TOKEN);
